cb0368ad5e35c6cab1f41201a2cb3cbff3f8de5a
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2025-04-25 23:19:03 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,14 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
+#echo "src-git customsd https://github.com/Ljzkirito/smartdns-openwrt.git;Release46-conf" >> "feeds.conf.default"
+
 # Remove 6in4
-#sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
-#sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
-#rm -fv package/emortal/ipv6-helper/files/60-6in4
+sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
+sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
+rm -fv package/emortal/ipv6-helper/files/60-6in4
+# Remove zram-swap
+sed -i 's/ zram-swap//g' target/linux/ramips/Makefile
+# Remove mwan3helper-chinaroute
+#sed -i 's/ luci-app-mwan3helper-chinaroute//g' target/linux/ramips/mt7621/target.mk
--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,26 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Replace Smartdns
-#./scripts/feeds uninstall luci-app-smartdns smartdns
-#./scripts/feeds install -a -p customsd
+# golang 1.24.x
+rm -rfv feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# Replace luci-app-ssr-plus & Depends
+Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client luci-app-ssr-plus"
+./scripts/feeds uninstall ${Replace_package}
+./scripts/feeds install -f -p helloworld ${Replace_package}
+
+# Replace shadowsocks-rust
+rm -rfv feeds/packages/net/shadowsocks-rust
+git clone https://github.com/sbwml/openwrt_helloworld openwrt_helloworld
+cp -rv openwrt_helloworld/shadowsocks-rust feeds/packages/net/shadowsocks-rust
+rm -rfv openwrt_helloworld
 
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# Remove upx commands
+makefile_file="$({ find package|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
+for a in ${makefile_file}
+do
+	[ -n "$(grep "upx" "$a")" ] && sed -i "/upx/d" "$a"
+done
